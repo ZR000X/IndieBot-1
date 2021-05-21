f44872fb113ec646f1fa96355c14593648baba9c
Complexor <jozbornn@gmail.com>
2021-05-21 15:01:08 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -7,7 +7,7 @@ from bot import create_bot_instance
 
 bot = create_bot_instance(".", 844015565243940864)
 
-print("Initializing")
+print("Initializing...")
 loop = get_event_loop()
 loop.run_until_complete(bot.start(inf.TOKEN))
 print("Bot loop complete.")
